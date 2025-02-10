d125a3bf4c1cad1b3be8387b11aeeea1e00d9ac4
Naufal Zufar S <naufal.zufar.s@gmail.com>
2025-02-10 08:07:11 +0700

    feat: create navbar ui

---
+++ src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav class="bg-[#fff]/05 w-full z-50 py-4">
+    <div class="mx-auto w-full max-w-7xl flex items-center justify-between px-4 lg:px-2 gap-10">
+      <div class="flex grow items-center gap-8">
+        <!-- <h1 class="text-2xl font-bold">Moovie</h1> -->
+        <img alt="MoovieTime logo" src="@/assets/moovie-time-logo.svg" width="112" height="31" />
+        <div
+          class="relative w-full bg-[#24282F] px-3 py-[6px] rounded-[4px] focus-within:ring-1 focus-within:ring-gray-600 flex gap-3 justify-between"
+        >
+          <div class="grow flex gap-3">
+            <img alt="movie icon" src="@/assets/icon-movie.svg" width="20" height="16" />
+            <input
+              type="text"
+              placeholder="Find movie"
+              class="cursor-pointer grow px-3 focus:outline-none bg-transparent text-white"
+            />
+          </div>
+          <img alt="search icon" src="@/assets/icon-search.svg" width="16" height="16" />
+        </div>
+      </div>
+      <ul class="flex items-center gap-9">
+        <li class="cursor-pointer uppercase hover:text-white">Categories</li>
+        <li class="cursor-pointer uppercase hover:text-white">Movies</li>
+        <li class="cursor-pointer uppercase hover:text-white">TV Shows</li>
+        <li class="cursor-pointer uppercase hover:text-white">Login</li>
+      </ul>
+    </div>
+  </nav>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import Navbar from '../components/Navbar.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="bg-[#292E36]">
+    <Navbar />
+  </div>
+  <main>hehe</main>
 </template>
