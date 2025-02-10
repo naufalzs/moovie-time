efd924e681764009609d0c6783dfb54ccf46b81b
Naufal Zufar S <naufal.zufar.s@gmail.com>
2025-02-10 14:09:41 +0700

    fix: minor ui

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -6,7 +6,7 @@
         alt=""
         class="inset-0 w-full h-full object-cover"
       />
-      <div class="absolute right-0 top-0 bg-[#1E232B]/80 px-2.5 py-1 text-white">7.0</div>
+      <div class="absolute right-0 top-0 bg-[#1E232B]/80 px-2.5 py-1 text-white font-bold">7.0</div>
       <div
         class="absolute inset-0 bg-[#2E2E2E] opacity-0 hover:opacity-100 transition-all duration-300 delay-100 flex flex-col items-center justify-center gap-10 text-white fonts-semibold"
       >
